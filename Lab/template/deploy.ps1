f8f0c473e2ca424ee8fffb117ab1d8f89c5c22ec
Krishna Khatri <krishnakhatri2003@gmail.com>
2026-06-30 09:23:39 +0530

    Updated SQL_Credentials

--- Lab/template/deploy.ps1
+++ Lab/template/deploy.ps1
@@ -530,6 +530,7 @@ function Save-SqlCredentialsToFile {
     $lines = @(
         "CreatedOnUtc: $((Get-Date).ToUniversalTime().ToString($CredentialsCreatedOnUtcFormat))",
         "SqlServerName: $SqlServerName",
+        "SqlServerFqdn: $SqlServerName.database.windows.net",
         "SqlDatabaseName: $SqlDatabaseName",
         "SqlAdminLogin: $SqlAdminLogin",
         "SqlAdminPassword: $SqlAdminPassword"
